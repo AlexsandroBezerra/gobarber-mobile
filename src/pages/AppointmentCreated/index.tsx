eb20a92dca2de23ff714414ae50da2b7f70aecde
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2020-12-16 11:24:11 -0300

    feat: add CreatedAppointment page

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
-import { View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React, { useCallback } from 'react'
+import Icon from 'react-native-vector-icons/Feather'
+
+import { Container, Title, Description, OkButton, OkButtonText } from './styles'
 
 const AppointmentCreated: React.FC = () => {
-  return <View />
+  const { reset } = useNavigation()
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0
+    })
+  }, [reset])
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+        consequatur? Culpa vitae praesentium similique unde modi, facilis at
+        sequi commodi eum reprehenderit aliquam repudiandae provident quae hic
+        ut id beatae.
+      </Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  )
 }
 
 export default AppointmentCreated
